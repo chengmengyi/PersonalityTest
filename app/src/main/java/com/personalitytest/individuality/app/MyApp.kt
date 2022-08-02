0ce88a2bee44ff7750383c3a2a4354917782db20
chengmengyi <243237049@qq.com>
2022-08-02 11:28:31 +0800

    a

--- app/src/main/java/com/personalitytest/individuality/app/MyApp.kt
+++ app/src/main/java/com/personalitytest/individuality/app/MyApp.kt
@@ -1,11 +1,14 @@
 package com.personalitytest.individuality.app
 
 import android.app.Application
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import com.personalitytest.individuality.helper.AllTypeHelper
 
 class MyApp:Application() {
     override fun onCreate() {
         super.onCreate()
         AllTypeHelper.readQuestionConf()
+        Firebase.initialize(this)
     }
 }
